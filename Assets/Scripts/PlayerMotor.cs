c220088170ef4eecdb69b4170ad09139ff4ebe9e
agwadan <agwa.dan@gmail.com>
2020-06-02 13:49:36 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour{
+
+    private CharacterController controller;
+    private Vector3 moveVector;
+    
+    private float speed = 5.0f;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
+
+    void Start(){
+        controller = GetComponent <CharacterController>();
+    }
+
+ 
+    void Update(){
+
+        moveVector = Vector3.zero;
+
+        if(controller.isGrounded){
+            verticalVelocity = -0.5f;//---------------------------------------- If you're on the floor, then you'll be pushed to the floor even more.
+        } else {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        //*****X********
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;//--------------- GetAxisRaw is sensitive to the gravity of the keys.
+
+        //*****Y********
+        moveVector.y = verticalVelocity;
+        //*****Z********
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);//---------------- Moving the character forward only...... Time.deltaTime is the time between two frames.
+    }
+}
