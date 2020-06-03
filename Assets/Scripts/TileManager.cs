d36f630141deb0a591af7b17d3527d833a142de0
agwadan <agwa.dan@gmail.com>
2020-06-03 20:34:03 +0300

    Added new paths and spawning new paths endlessly.

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour{
+
+    public GameObject [] tilePrefabs;
+    private Transform playerTransform;
+    private float spawnZ = 0.0f;
+    private float tileLength = 10.0f;
+    private int amtOfTilesOnScreen = 7;
+
+    void Start(){
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        for(int i = 0; i < amtOfTilesOnScreen ; i++){//------------------------- Spawning the first set of 7 tiles.
+            SpawnTile();
+        }
+    }
+
+    void Update(){
+        if(playerTransform.position.z > (spawnZ - amtOfTilesOnScreen * tileLength)){
+            SpawnTile();
+        }
+    }
+
+    private void SpawnTile(int prefabIndex = -1){ //----------------------------- Function to spawn tiles.
+        GameObject go;
+        go = Instantiate (tilePrefabs[0]) as GameObject;
+        go.transform.SetParent (transform);//------------------------------------ Setting the new tile to the position of it's parent which is the tile manager.
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ = spawnZ + tileLength;
+    }
+}
