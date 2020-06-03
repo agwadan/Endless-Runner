579b2acd9e8922d0e89f0a87b4a772d2a1a9cc00
agwadan <agwa.dan@gmail.com>
2020-06-04 00:58:53 +0300

    Randomizing tiles and deleting the ones passed.

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -6,29 +6,68 @@ public class TileManager : MonoBehaviour{
 
     public GameObject [] tilePrefabs;
     private Transform playerTransform;
-    private float spawnZ = 0.0f;
+    private float spawnZ = -12.0f;//------------------------------------------- The value is negative so the camera doesn't see the empty space at the beginning of the run.
     private float tileLength = 10.0f;
+    private float safeZone = 15.0f;
     private int amtOfTilesOnScreen = 7;
+    private int lastPrefabIndex = 0;
+    private List<GameObject> activeTiles;
 
     void Start(){
+        activeTiles = new List<GameObject>();
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 
         for(int i = 0; i < amtOfTilesOnScreen ; i++){//------------------------- Spawning the first set of 7 tiles.
-            SpawnTile();
+            if(i < 2){
+                SpawnTile(0);
+            } else {
+                SpawnTile();
+            }
         }
     }
 
     void Update(){
-        if(playerTransform.position.z > (spawnZ - amtOfTilesOnScreen * tileLength)){
+        if(playerTransform.position.z - safeZone> (spawnZ - amtOfTilesOnScreen * tileLength)){
             SpawnTile();
+            DeleteTile();
         }
     }
 
     private void SpawnTile(int prefabIndex = -1){ //----------------------------- Function to spawn tiles.
         GameObject go;
-        go = Instantiate (tilePrefabs[0]) as GameObject;
+
+        if(prefabIndex == -1){
+             go = Instantiate (tilePrefabs[RandomPrefabIndex()]) as GameObject;
+        } else {
+             go = Instantiate (tilePrefabs[prefabIndex]) as GameObject;
+        }
+       
         go.transform.SetParent (transform);//------------------------------------ Setting the new tile to the position of it's parent which is the tile manager.
         go.transform.position = Vector3.forward * spawnZ;
         spawnZ = spawnZ + tileLength;
+        activeTiles.Add (go); //------------------------------------------------- Adding the gameObject to the list of active tiles.
+    }
+
+    private void DeleteTile(){
+        Destroy (activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+    private int RandomPrefabIndex(){
+        if(tilePrefabs.Length <=1){
+                return 0;
+            }
+
+            int randomIndex = lastPrefabIndex;
+            while(randomIndex == lastPrefabIndex){ //--------------------------- If the random index chosen is the same as the previous, the while loop is repeated.
+                randomIndex = Random.Range (0, tilePrefabs.Length);
+
+            }
+
+            lastPrefabIndex = randomIndex;
+            
+        
+
+        return randomIndex;
     }
 }
